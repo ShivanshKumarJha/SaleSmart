0a3c8290ce74bef49956a8cb90a6db0cce3d080c
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2025-01-30 02:05:24 +0530

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {AuthProvider} from "./contexts/AuthContext.jsx";
-import {ProductContextProvider} from "./contexts/ProductContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <AuthProvider>
-            <ProductContextProvider>
-                <App/>
-            </ProductContextProvider>
+            <App/>
         </AuthProvider>
     </StrictMode>,
 )
