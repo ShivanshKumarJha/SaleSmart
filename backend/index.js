d183a215792d1d4e229dc9657056e93f9bf489d3
Shivansh Kumar Jha <kumarjhashivansh@gmail.com>
2025-07-10 15:38:02 +0530

    update bug#2

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - Session ID: ${req.session.id}`);
+  console.log(`${req.method} ${req.path}`);
   next();
 });
 
